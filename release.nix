b0e8ce14568d9025a511bd2c5dabc2a630819859
- <jeff@haste.us>
2017-07-19 21:00:42 -0400

    Hydra test

---
+++ release.nix
@@ -0,0 +1,17 @@
+let
+  pkgs = import <nixpkgs> {};
+
+  jobs = rec {
+
+    build =
+      { system ? builtins.currentSystem }: 
+
+      let pkgs = import <nixpkgs> { inherit system; }; in
+      pkgs.releaseTools.nixBuild {
+        name = "sbtix";
+        src = <githubSource>;
+        buildInputs = [pkgs];
+      };
+  };
+in
+  jobs
