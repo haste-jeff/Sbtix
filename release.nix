f2111b5ecdce0dee6feae62eca6268d892c82727
- <jeff@haste.us>
2017-07-19 22:16:05 -0400

    .

--- release.nix
+++ release.nix
@@ -1,17 +1,13 @@
 let
   pkgs = import <nixpkgs> {};
-  default = import ./default.nix;
   
   jobs = rec {
 
     build =
-      { system ? builtins.currentSystem }: 
-
-      let pkgs = import <nixpkgs> { inherit system; }; in
       pkgs.releaseTools.nixBuild {
         name = "sbtix";
         src = <githubSource>;
-        buildInputs = [ default ];
+        buildInputs = [ ./default.nix ];
       };
   };
 in
